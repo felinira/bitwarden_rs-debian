45413efe0b2c5fd163081b0fd380719af12b59fe
felinira <code@felinira.net>
2020-07-06 02:28:14 +0200

    Remove dependency on docker completely

--- README.md
+++ README.md
@@ -3,21 +3,25 @@
 [Bitwarden_rs](https://github.com/dani-garcia/bitwarden_rs) is an "Unofficial Bitwarden compatible server written in Rust".
 
 This repository will help you produce a debian package.
+This fork of greizgh/bitwarden_rs-debian does not use any docker dependencies. Thus it is ideal for people who *really* dislike docker :)
 
 ## TL;DR
 
 Make sure you have the required build dependencies:
-* docker
-* git
-* patch
-* curl
+```
+curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+sudo apt update
+sudo apt install -y nodejs build-essential pkg-config libssl-dev git openssl ca-certificates curl sqlite3
+```
 
 Then:
 
 ```
-git clone https://github.com/greizgh/bitwarden_rs-debian.git
+git clone https://github.com/felinira/bitwarden_rs-debian.git
 cd bitwarden_rs-debian
-./build.sh -r 1.10.0
+./build.sh # latest version
+./build.sh -r 1.10.0 # specific version
 ```
 
 The `build.sh` script will build bitwarden_rs for the same Debian version which targets bitwarden_rs.
--- build.sh
+++ build.sh
@@ -3,7 +3,8 @@
 set -e
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-SRC="$DIR/git"
+SRC="$DIR/bitwarden_rs_git"
+SRC_WEB="$DIR/bw_web_builds"
 DST="$DIR/dist"
 
 while getopts ":r:o:d:" opt; do
@@ -19,6 +20,7 @@ while getopts ":r:o:d:" opt; do
   esac
 done
 if [ -z "$REF" ]; then REF=$(curl -s https://api.github.com/repos/dani-garcia/bitwarden_rs/releases/latest | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/' | cut -c 1-); fi
+if [ -z "$REF_WEB" ]; then REF_WEB=$(curl -s https://api.github.com/repos/dani-garcia/bw_web_builds/releases/latest | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/' | cut -c 1-); fi
 if [ -z "$OS_VERSION_NAME" ]; then OS_VERSION_NAME='buster'; fi
 if [ -z "$DB_TYPE" ]; then DB_TYPE="sqlite"; fi
 
@@ -29,8 +31,23 @@ fi
 cd "$SRC" || exit
 CREF="$(git branch | grep \* | cut -d ' ' -f2)"
 if [ "$CREF" != "$REF" ]; then
-  git fetch
+  git fetch --tags
   git checkout "$REF" --force
+else
+  git clean -d -f
+  git pull
+fi
+cd - || exit
+
+# Clone bitwarden_web patches
+if [ ! -d "$SRC_WEB" ]; then
+  git clone https://github.com/dani-garcia/bw_web_builds.git
+fi
+cd "$SRC_WEB" || exit
+CREF="$(git branch | grep \* | cut -d ' ' -f2)"
+if [ "$CREF" != "$REF_WEB" ]; then
+  git fetch --tags
+  git checkout "$REF_WEB" --force
 else
   git clean -d -f
   git pull
@@ -48,9 +65,6 @@ sed -i "s/Uncomment any of the following lines to change the defaults/Uncomment 
 mkdir -p "$DST"
 
 # Prepare Dockerfile
-patch -i "$DIR/Dockerfile.patch" "$SRC/docker/amd64/$DB_TYPE/Dockerfile" -o "$DIR/Dockerfile" || exit
-sed -E "s/(FROM[[:space:]]*rust:)[^[:space:]]+(.+)/\1${OS_VERSION_NAME}\2/g" -i "$DIR/Dockerfile"
-sed -E "s/(FROM[[:space:]]*debian:)[^-]+(-.+)/\1${OS_VERSION_NAME}\2/g" -i "$DIR/Dockerfile"
 
 # Prepare Systemd-unit
 SYSTEMD_UNIT="$DIR/debian/bitwarden_rs.service"
@@ -60,8 +74,34 @@ elif [ "$DB_TYPE" = "postgresql" ]; then
   sed -i "s/After=network.target/After=network.target postgresql.service\nRequires=postgresql.service/g" "$SYSTEMD_UNIT"
 fi
 
-docker build -t bitwarden-deb "$DIR"
+cd "$SRC"
+if [ "$DB_TYPE" = "mysql" ]; then
+cargo build --features mysql --release
+elif [ "$DB_TYPE" = "postgresql" ]; then
+cargo build --features postgresql --release
+else
+cargo build --features sqlite --release
+fi
 
-CID=$(docker run -d bitwarden-deb)
-docker cp "$CID":/bitwarden_package/bitwarden-rs.deb "$DST/bitwarden_rs-${OS_VERSION_NAME}-${REF}-${DB_TYPE}.deb"
-docker rm "$CID"
+cd "$SRC_WEB"
+VAULT_VERSION="$REF_WEB" ./package_web_vault.sh
+
+cd "$DIR"
+rm -rf bitwarden_package
+mkdir -p bitwarden_package/DEBIAN
+mkdir -p bitwarden_package/usr/bin
+mkdir -p bitwarden_package/usr/lib/systemd/system
+mkdir -p bitwarden_package/etc/bitwarden_rs
+mkdir -p bitwarden_package/usr/share/bitwarden_rs
+
+cp "debian/control" "bitwarden_package/DEBIAN/control"
+cp "debian/postinst" "bitwarden_package/DEBIAN/postinst"
+cp "debian/conffiles" "bitwarden_package/DEBIAN/conffiles"
+cp "$SRC/Rocket.toml" "bitwarden_package/etc/bitwarden_rs"
+cp "debian/config.env" "bitwarden_package/etc/bitwarden_rs"
+cp "debian/bitwarden_rs.service" "bitwarden_package/usr/lib/systemd/system"
+cp -r "$SRC_WEB/web-vault/build" "bitwarden_package/usr/share/bitwarden_rs/web-vault"
+cp -r "$SRC/target/release/bitwarden_rs" "bitwarden_package/usr/bin/"
+
+cd "$DIR/bitwarden_package"
+dpkg-deb --build . bitwarden-rs.deb
